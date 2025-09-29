88fed8e2f359269176a3b0005c8c71feaf1d64c1
Minahil yaqoob <24011519-031@uog.edu.pk>
2025-09-29 09:48:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+let btn = document.querySelector("form button");
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
+let msg = document.querySelector(".msg");
+
+for (let select of dropdown) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "PKR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+const updateExchangeRate = async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  try {
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalAmount = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal}${fromCurr.value} = ${finalAmount}${toCurr.value}`;
+  } catch {
+    console.log("error");
+  }
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate(evt);
+});
+
+window.addEventListener("load", (evt) => {
+  updateExchangeRate(evt);
+});
